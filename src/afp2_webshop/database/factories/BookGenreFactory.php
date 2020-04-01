31c0c5852e08f90c4a1e051a1ddc164ecdef5b82
Bognar Viktoria <apalosaa4@gmail.com>
2020-04-01 22:10:37 +0200

    Book_genre.php - added.

---
+++ src/afp2_webshop/database/factories/BookGenreFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Book_genre;
+use Faker\Generator as Faker;
+
+$factory->define(Book_genre::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
