89eddaee9fddf67eb13da0e1d67f8ef1bf42470e
Dombi Tibor <dombidav@gmail.com>
2020-04-02 12:39:26 +0200

    Fixed author factory

--- src/afp2_webshop/database/factories/AuthorFactory.php
+++ src/afp2_webshop/database/factories/AuthorFactory.php
@@ -7,7 +7,6 @@ use Faker\Generator as Faker;
 
 $factory->define(Author::class, function (Faker $faker) {
     return [
-        'id' => $faker->numerify('###'),
         'name' => $faker->name
     ];
 });
