9a6c821e0c708173a04952a39e86121f54df347e
Regina <urbanregina12@gmail.com>
2020-04-11 16:47:47 +0200

    BookTest.php - kész

---
+++ src/afp2_webshop/tests/Unit/BookTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Book;
+//use PHPUnit\Framework\TestCase;
+use Illuminate\Support\Facades\DB;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+//use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class BookTest extends TestCase
+{
+
+    public function setUp(): void
+    {
+        parent::setUp();
+    }
+
+    public function testIfBookStatusIsOkay()
+    {
+        $response = $this->get('/shop');
+        $response->assertStatus(200);
+    }
+
+    public function testMultipleBookShow(){
+        $response = $this->get('/shop');
+        $response->assertStatus(200);
+
+        $content = json_decode($this->get('/shop')->content());
+        $this->assertNotEmpty($content[1]->id);
+    }
+
+    public function testSingleBookShow(){
+        $response = $this->get('/shop/5');
+        $response->assertStatus(200);
+
+        $content = json_decode($this->get('/shop/5')->content());
+        $this->assertNotEmpty($content->id);
+    }
+
+    public function testBookFactory(){
+        $table_count = Book::all()->count();
+        factory(Book::class)->create();
+
+        $this->assertEquals($table_count + 1, Book::all()->count());
+    }
+}
