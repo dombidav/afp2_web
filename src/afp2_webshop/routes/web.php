d31e855402eaf1a821a93d1d7bd589c0066edeb7
Dombi Tibor <dombidav@gmail.com>
2020-04-16 01:13:50 +0200

    routes optimized

--- src/afp2_webshop/routes/web.php
+++ src/afp2_webshop/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,37 +14,33 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'StaticController@showHome');
-Route::get('/contact/', 'StaticController@showContact');
-Route::get('/about/', 'StaticController@showAbout');
-/*
- * GET - "Kérem" > könyv adatai
- * POST - "Adom" > új könyv
- * PUT - "Átírom" > módosítom
- * DELETE - "Törlöm"
- */
+Route::get('/', 'StaticController@showHome')->name('main');
+Route::get('/contact', 'StaticController@showContact')->name('contact');
+Route::get('/about', 'StaticController@showAbout')->name('about');
 
-Route::get('/shop', 'BookController@index');
-Route::get('/shop/{id}', 'BookController@show');
+Route::get('/shop', 'BookController@index')->name('shop');
+Route::post('/shop', 'BookController@search')->name('shop.search');
+Route::get('/shop/{id}', 'BookController@show')->name('shop.get');
 
-Route::get('/cart', 'CartController@index');
-Route::get('/cart/{id}', 'CartController@show');
-Route::get('/cart/add/{id}', 'CartController@add');
-Route::get('/cart/add/{book_id}/{user_id}', 'CartController@add2');
-Route::get('/cart/remove/{id}', 'CartController@remove');
-Route::get('/cart/remove/{book_id}/{user_id}', 'CartController@remove2');
-Route::get('/cart/edit/{book_id}/{quantity}', 'CartController@edit');
-Route::get('/cart/edit/{book_id}/{quantity}/{user_id}', 'CartController@edit2');
+Route::get('/cart', 'CartController@index')->name('cart');
+Route::get('/cart/{id}', 'CartController@show')->name('cart-id');
+Route::get('/cart/add/{id}', 'CartController@add')->name('cart.add');
+Route::get('/cart/add/{book_id}/{user_id}', 'CartController@add2')->name('cart.add-user');
+Route::get('/cart/remove/{id}', 'CartController@remove')->name('cart.remove');
+Route::get('/cart/remove/{book_id}/{user_id}', 'CartController@remove2')->name('cart.remove-user');
+Route::get('/cart/edit/{book_id}/{quantity}', 'CartController@edit')->name('cart.edit');
+Route::get('/cart/edit/{book_id}/{quantity}/{user_id}', 'CartController@edit2')->name('cart.edit-user');
 
-Route::get('/author', 'AuthorController@index');
-Route::get('/author/{id}', 'AuthorController@show');
+Route::get('/author', 'AuthorController@index')->name('author');
+Route::get('/author/{id}', 'AuthorController@show')->name('author.get');
 
-Route::get('/publisher', 'publisherController@index');
-Route::get('/publisher/{id}', 'publisherController@show');
+Route::get('/publisher', 'publisherController@index')->name('publisher');
+Route::get('/publisher/{id}', 'publisherController@show')->name('publisher.get');
 
-Route::get('/genre', 'publisherController@index');
-Route::get('/genre/{id}', 'publisherController@show');
+Route::get('/genre', 'publisherController@index')->name('genre');
+Route::get('/genre/{id}', 'publisherController@show')->name('genre.get');
 
 Auth::routes();
-Route::get('/profile', 'ProfileController@show');
-Route::get('/logout', '\App\Http\Controllers\Auth\LoginController@logout');
+Route::get('/profile', 'ProfileController@show')->name('profile');
+Route::get('/signout', '\App\Http\Controllers\Auth\LoginController@logout')->name('signout');
+
