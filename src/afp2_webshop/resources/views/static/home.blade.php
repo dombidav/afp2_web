f7a55afd5a529f42aab4d8ecc35c131d925db9d2
Dombi Tibor <dombidav@gmail.com>
2020-04-16 18:04:13 +0200

    Layout

--- src/afp2_webshop/resources/views/static/home.blade.php
+++ src/afp2_webshop/resources/views/static/home.blade.php
@@ -1,9 +1,13 @@
-Newest:<br>
-@foreach($newest as $new)
-    -> {{ $new }}<br>
-@endforeach
-<hr>
-Featured:<br>
-@foreach($featured as $book)
-    -> {{ $book }}<br>
-@endforeach
+@extends('layouts.app')
+
+@section('content')
+    Newest:<br>
+    @foreach($newest as $new)
+        -> {{ $new }}<br>
+    @endforeach
+    <hr>
+    Featured:<br>
+    @foreach($featured as $book)
+        -> {{ $book }}<br>
+    @endforeach
+@endsection
