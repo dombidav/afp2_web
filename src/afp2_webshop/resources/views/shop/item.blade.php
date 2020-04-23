5db290e444447e7d2be3e50e1ed659f221307f10
Regina <urbanregina12@gmail.com>
2020-04-23 19:37:13 +0200

    shop/item.blade.php - static html added

--- src/afp2_webshop/resources/views/shop/item.blade.php
+++ src/afp2_webshop/resources/views/shop/item.blade.php
@@ -2,4 +2,167 @@
 
 @section('content')
     {{ $book }}
+    <section class="section-name padding-y-sm">
+        <div class="container">
+            <div class="row justify-content-center">
+                <table class="table table-borderless text-md-center col-8">
+                    <thead>
+                    </thead>
+
+                    <tbody>
+                    <tr class="bg">
+                        <th scope="row"> <a href="#" class="img-wrap"> <img src="/images/unknown_product.png" class="img-sm"> </a>
+                        </th>
+                        <th>
+                            The title of the book
+                        </th>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+
+            <div class="row justify-content-center">
+                <table class="table table-borderless text-md-center col-2">
+                    <tbody>
+                    <tr class="table table-borderless text-md-center col-3">
+                        <th>
+                        </th>
+                    </tr>
+                    </tbody>
+                    <tbody>
+                    <tr class="bg table table-borderless text-md-center col-3">
+                        <th>Author
+                        </th>
+                    </tr>
+                    </tbody>
+                    <tbody>
+                    <tr class="bg table table-borderless text-md-center col-3">
+                        <td>Someone
+                        </td>
+                    </tr>
+                    </tbody>
+                    <tbody>
+                    <tr class="bg table table-borderless text-md-center col-3">
+                        <th>
+                        </th>
+                    </tr>
+                    </tbody>
+                    <tbody>
+                    <tr class="bg table table-borderless text-md-center col-3">
+                        <th>Publisher
+                        </th>
+                    </tr>
+                    </tbody>
+                    <tbody>
+                    <tr class="bg table table-borderless text-md-center col-3">
+                        <td>Someone
+                        </td>
+                    </tr>
+                    </tbody>
+                    <tbody>
+                    <tr class="bg table table-borderless text-md-center col-3">
+                        <th>
+                        </th>
+                    </tr>
+                    </tbody>
+                    <tbody>
+                    <tr class="bg table table-borderless text-md-center col-3">
+                        <th>Genre
+                        </th>
+                    </tr>
+                    </tbody>
+                    <tbody>
+                    <tr class="bg table table-borderless text-md-center col-3">
+                        <td>Something
+                        </td>
+                    </tr>
+                    </tbody>
+                    <tbody>
+                    <tr class="bg table table-borderless text-md-center col-3">
+                        <th>
+                        </th>
+                    </tr>
+                    </tbody>
+                    <tbody>
+                    <tr class="bg table table-borderless text-md-center col-3">
+                        <th>Language
+                        </th>
+                    </tr>
+                    </tbody>
+                    <tbody>
+                    <tr class="bg table table-borderless text-md-center col-3">
+                        <td>Something
+                        </td>
+                    </tr>
+                    </tbody>
+                    <tbody>
+                    <tr class="bg table table-borderless text-md-center col-3">
+                        <th>
+                        </th>
+                    </tr>
+                    </tbody>
+                    <tbody>
+                    <tr class="bg table table-borderless text-md-center col-3">
+                        <td>Price:
+                        </td>
+                    </tr>
+                    </tbody>
+                    <tbody>
+                    <tr class="bg table table-borderless text-md-center col-3">
+                        <td>XXXX Ft
+                        </td>
+                    </tr>
+                    </tbody>
+                    <tbody>
+                    <tr class="bg table table-borderless text-md-center col-3">
+                        <td>
+                            <a href="#" class="btn btn-block btn-warning">Add to cart </a>
+                        </td>
+                    </tr>
+                    </tbody>
+
+                    <div class="row justify-content-center">
+                        <table class="table table-borderless text-md-center col-5">
+                            <tbody>
+                            <tr class="table table-borderless text-md-center col-8">
+                                <th>
+                                </th>
+                            </tr>
+                            </tbody>
+                            <tbody>
+                            <tr class="table table-borderless text-md-center col-8">
+                                <th>
+                                    Description
+                                </th>
+                            </tr>
+                            </tbody>
+                            <tbody>
+                            <tr class="table table-borderless text-md-center col-8">
+                                <td>
+                                </td>
+                            </tr>
+                            </tbody>
+                            <tbody>
+                            <tr class="table table col-md-8 table-borderless text-md-center col-8">
+                                <td>
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+
+                                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+
+                                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                                </td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </table>
+            </div>
+        </div>
+    </section>
+
 @endsection('content')
