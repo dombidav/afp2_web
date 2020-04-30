cfd424301781473cb1111df26f7fe17da8f5cd54
Dombi Tibor <dombidav@gmail.com>
2020-04-30 20:10:13 +0200

    address factory modified

--- src/afp2_webshop/database/factories/AddressesFactory.php
+++ src/afp2_webshop/database/factories/AddressesFactory.php
@@ -6,9 +6,10 @@ use App\Addresses;
 use Faker\Generator as Faker;
 
 $factory->define(Addresses::class, function (Faker $faker) {
+    $tin = rand(0,1) == 0 ? null : $faker->numerify('##########');
     return [
         'country' => $faker->numberBetween(0,100),
-        'tin' => $faker->lexify('000000000'),
+        'tin' => $tin,
         'postal_code' => $faker->lexify('0000'),
         'city' => $faker->city,
         'street' => $faker->address,
