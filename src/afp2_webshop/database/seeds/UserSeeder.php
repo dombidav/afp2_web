f882b0e652366f8928f71291f4bd5b11728010df
Dombi Tibor <dombidav@gmail.com>
2020-04-30 18:12:40 +0200

    User Seeder

---
+++ src/afp2_webshop/database/seeds/UserSeeder.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = \Faker\Factory::create();
+
+        DB::table('users')
+            ->insert([
+                'name' => 'TEST USER',
+                'date_of_birth' => $faker->date('Y-m-d'),
+                'gender' => 0,
+                'email' => 'test@example.com',
+                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+                'billing' => 0,
+                'shipping' => 0
+            ]);
+
+        for($i=0; $i<100; $i++){
+
+            $hasShipping = rand(0, 2);
+            $shipping = null;
+            $billing = null;
+            if($hasShipping > 0){
+                $shipping = factory(\App\Addresses::class)->create()->id;
+                if($hasShipping > 1){
+                    $billing = factory(\App\Addresses::class)->create()->id;
+                }
+            }
+
+            DB::table('users')
+                ->insert([
+                    'name' => $faker->name,
+                    'date_of_birth' => $faker->date('Y-m-d'),
+                    'gender' => rand(0,2),
+                    'email' => $faker->email,
+                    'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+                    'billing' => $billing,
+                    'shipping' => $shipping
+                ]);
+        }
+    }
+}
