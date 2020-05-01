354d68ef8abfc5b658b0e0960fe64da17cf9123e
MartonPeterMarton <martonpeter95@gmail.com>
2020-05-01 19:35:26 +0200

    profile and editprofile and ordersBase
    I did what i could with the profile views...

--- src/afp2_webshop/resources/views/profile/editprofile.blade.php
+++ src/afp2_webshop/resources/views/profile/editprofile.blade.php
@@ -1,5 +1,55 @@
 @extends('layouts.app')
 
 @section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-xs-12 col-sm-6 col-md-6">
+             <div class="col-sm-6 col-md-8">
+                        <h2> Your Profile   
+                        </h2>
+                    <div class="table responsive">
+                       <table> 
+                       
+                       <t>
+                       <tr>Name: <input id="name" type="text" name="name"></tr>
+                       </br>
+                       <tr>Email: <input id="email" type="email" email="email"></tr>
+                       </br>
+                            <tr>Gender: <select id="genders">
+                                <option value="male">Male</option>
+                                <option value="female">Female</option>
+                                <option value="other">Other</option>
+                                </select>
+                            </tr>
+                       </br>
+                       <tr>Date of birth: <input id="dateofbirth" type="date" date="dateofbirth"></tr>
+                       </br>
+                       <div id=billingaddress><tr>Billing address: <input id="billing" type="text" billing="billing"></tr></div><!--FOR TIBI-->
+                       </br>
+                        <div id=shippingaddress><tr>Shipping address: <input id="shipping" type="text" shipping="shipping"></tr></div><!--FOR TIBI-->
+                       </t>
+                       <?php
+                        // WHY THE FUCK IS THIS SHIT NOT WORKING!?
+                         function index(){ 
+                            $affected = DB::update('update users  where name = ?', ['name']);
+                         //$users = DB::select('select * from users where id = :id', ['id' => 1]);
+                       //$results = DB::select('select * from users where id = :id', ['id' => 1]);
+                       return view('user.index', ['users' => $users]);
+                            }
 
+                    ?>
+                       </table>
+                    </div>
+
+                        </br>
+                        <div class="btn-group">
+                        <button type="button" class="btn-warning">
+                      Save Profile Changes</button> <!--This button should save the data and redirect the user to it's profile-->
+                </div>
+            </div>
+                
+            
+        </div>
+    </div>
+</div>
 @endsection
--- src/afp2_webshop/resources/views/profile/profile.blade.php
+++ src/afp2_webshop/resources/views/profile/profile.blade.php
@@ -4,28 +4,39 @@
 <div class="container">
     <div class="row">
         <div class="col-xs-12 col-sm-6 col-md-6">
-          
-               
-                   
-                    <div class="col-sm-6 col-md-8">
-                        <h4> Username
-                            <!--USERNAME COMES HERE-->
-                        </h4>
-                        <p>
-                        Actual name of the person
-                        </p>
-
-                        <small><cite title="address">Eger, HUN 
-                        <!--ADDRESS COME HERE IF THE USER HAS GIVEN US AT ALL-->
-                        </cite></small>
-                        
-                        <p>
-                            <i class="email"></i> <!--EMAIL COMES HERE-->email@example.com
-                            <br/>
-                            
-                        </p>
+             <div class="col-sm-6 col-md-8">
+                        <h2> Your Profile   
+                        </h2>
+                    <div class="table responsive">
+                       <table> 
                        
-                        <div class="">
+                       <t>
+                       <tr>Name: </tr>
+                       </br>
+                       <tr>Email: </tr>
+                       </br>
+                       <tr>Gender: </tr>
+                       </br>
+                       <tr>Date of birth: </tr>
+                       </br>
+                       <div id=billingaddress><tr>Billing address: </tr></div><!--FOR TIBI-->
+                       </br>
+                        <div id=shippingaddress><tr>Shipping address: </tr></div><!--FOR TIBI-->
+                       </t>
+                       <?php
+                        // WHY THE FUCK IS THIS SHIT NOT WORKING!?
+                         function index(){ 
+                         $users = DB::select('select * from users where id = :id', ['id' => 1]);
+                       //$results = DB::select('select * from users where id = :id', ['id' => 1]);
+                       return view('user.index', ['users' => $users]);
+                            }
+
+                    ?>
+                       </table>
+                    </div>
+                        <p>
+
+                        <div class="btn">
                             <button type="button" class="btn btn-warning">Orders
                             </button><!--This should redirect the user to his/her orders and show said orders status and list the orders-->
                         </div>
@@ -33,8 +44,8 @@
                         <div class="btn-group">
                         <button type="button" class="btn-warning"> <!--This button looks different from the others for the purpose of the users not mixing them up-->
                         Edit Profile</button> <!--This button should enable the logged in user to edit his/her profile-->
-                        </div>
-                    </div>
+                </div>
+            </div>
                 
             
         </div>
